d89b613114c59d6d5cd80cf01441fe6a5e38d57d
adarshb <chateadarsh7777@gmail.com>
2025-07-17 10:45:17 +0530

    new changes

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -39,4 +39,13 @@ api.interceptors.response.use(
   }
 );
 
+// Context API instance for context lookup backend
+export const contextApi = axios.create({
+  baseURL: 'http://localhost:4000/api',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
 export default api;
